f3b87c8b8602bc64667ce1f4a1d09ac0ab327410
therealaswrek <65848860+TheRealAswrek@users.noreply.github.com>
2026-07-28 01:02:11 +0200

    added preview of written input

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,67 @@ const float TILE_SIZE = 48.f;
 
 class UiKeyboard {
 	public:
+		const std::string LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		const int COLUMNS = 3;
+		std::string content = "";
+
 		float cursor_x;
 		float cursor_y;
+
+		std::string currentlyHovering = "";
 		
+		float cursor_rx = cursor_x; //r means render (to lerp)
+		float cursor_ry = cursor_y;
+
+		void checkCursor(){
+			int index = static_cast<int>((cursor_y - 1) * COLUMNS + (cursor_x - 1));
+
+			if (index >= 0 && index < LETTERS.size())
+			{
+    			currentlyHovering = LETTERS[index];
+			}
+			else
+			{
+    			currentlyHovering = "OK";
+			}
+		}
+
 		void checkKeyboardInput(sf::Keyboard::Key keyCode){
 			if (keyCode == sf::Keyboard::Key::Left) cursor_x -= 1; 
 			if (keyCode == sf::Keyboard::Key::Right) cursor_x += 1; 
 			if (keyCode == sf::Keyboard::Key::Up) cursor_y -= 1; 
 			if (keyCode == sf::Keyboard::Key::Down) cursor_y += 1;
+			if (keyCode == sf::Keyboard::Key::Enter || keyCode == sf::Keyboard::Key::Space){
+				if (currentlyHovering == "OK" || content.length() >= 10) return;
+
+				content += currentlyHovering;
+			}
+			if (keyCode == sf::Keyboard::Key::Backspace){
+				content = content.substr(0,content.length()-1);
+			}
 
 			if (cursor_x > 3) cursor_x = 1;
 			if (cursor_x < 1) cursor_x = 3;
 			if (cursor_y > 9) cursor_y = 1;
 			if (cursor_y < 1) cursor_y = 9;
+
+			checkCursor();
 		}
 
 		void resetCursorPosition(){
 			cursor_x = 1;
 			cursor_y = 1;
+			cursor_rx = 1;
+			cursor_ry = 1;
 		};
 
 		void render(sf::RenderWindow &window, sf::Font font){
-			std::string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 			
-			const int COLUMNS = 3;
 
 			float column = 1;
 			float row = 1;
-			for (int i = 1; i <= letters.length(); i++){
-				char key = letters[i-1];
+			for (int i = 1; i <= LETTERS.length(); i++){
+				char key = LETTERS[i-1];
 				sf::Text letter(font);
 				letter.setString(key);
 				letter.setFillColor(sf::Color::White);
@@ -96,6 +128,7 @@ int main()
 
 	UiKeyboard virtualKeyboard;
 	virtualKeyboard.resetCursorPosition();
+	virtualKeyboard.checkCursor();
 
 
 	sf::RenderWindow mainWindow( sf::VideoMode( { 640, 512 } ), "wyprawaRPG", sf::Style::Titlebar | sf::Style::Close);
@@ -146,21 +179,33 @@ int main()
 	sf::RectangleShape keyboardCursor({TILE_SIZE, TILE_SIZE});
 	keyboardCursor.setFillColor(sf::Color::Transparent);
 	keyboardCursor.setOutlineColor(sf::Color::Blue);
-	keyboardCursor.setOutlineThickness(2.f);
-
-	keyboardCursor.setPosition({virtualKeyboard.cursor_x * TILE_SIZE ,virtualKeyboard.cursor_y * TILE_SIZE});
-	mainWindow.draw(keyboardCursor);
+	keyboardCursor.setOutlineThickness(3.f);
 
 	virtualKeyboard.render(mainWindow, GENERAL_FONT);
 
+	virtualKeyboard.cursor_rx = lerp(virtualKeyboard.cursor_rx,virtualKeyboard.cursor_x,0.5f);
+	virtualKeyboard.cursor_ry = lerp(virtualKeyboard.cursor_ry,virtualKeyboard.cursor_y,0.5f);
+	keyboardCursor.setPosition({virtualKeyboard.cursor_rx * TILE_SIZE ,virtualKeyboard.cursor_ry * TILE_SIZE});
+	mainWindow.draw(keyboardCursor);
+
+	sf::Text inputContent(GENERAL_FONT,virtualKeyboard.content);
+	inputContent.setCharacterSize(48);
+	sf::FloatRect bounds = inputContent.getLocalBounds();
+	inputContent.setOrigin({
+		bounds.position.x + bounds.size.x / 2,
+		bounds.position.y + bounds.size.y / 2
+	});
+	inputContent.setPosition({TILE_SIZE*8.5,TILE_SIZE*3});
+	mainWindow.draw(inputContent);
+
+	/*
 	sf::Text debugText(GENERAL_FONT);	
-	debugText.setString("x:"+std::to_string(int(virtualKeyboard.cursor_x)) + "\ny:" + std::to_string(int(virtualKeyboard.cursor_y)));
+	debugText.setString("x:"+std::to_string(virtualKeyboard.cursor_rx) + "\ny:" + std::to_string(virtualKeyboard.cursor_ry) + "\ncurrently hovering on: "+ virtualKeyboard.currentlyHovering + "\nContent:"+ virtualKeyboard.content);
 	debugText.setCharacterSize(14);
 	debugText.setFillColor(sf::Color::White);
 	debugText.setStyle(sf::Text::Bold);
 	mainWindow.draw(debugText);
-
-	
+	*/
 
 	mainWindow.display();
 	}
