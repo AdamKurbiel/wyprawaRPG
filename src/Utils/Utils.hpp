106f88fc4125e0ba8cd4a422757bde88d0e9740e
therealaswrek <65848860+TheRealAswrek@users.noreply.github.com>
2026-07-29 13:28:46 +0200

    reworked Utils.hpp

--- src/Utils/Utils.hpp
+++ src/Utils/Utils.hpp
@@ -1,5 +1,18 @@
-#pragma once
+/*
+<Copyright Adam Kurbiel 2026>
 
-float lerp(float start, float end, float t){
-    return start * (1 - t) + end * t;
+Contains utility functions.
+*/
+
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+namespace utils {
+    template <typename T>
+    constexpr inline T lerp(T a, T b, T t) noexcept {
+        return (static_cast<T>(1) - t) * a + t * b;
+    }
 }
+
+#endif
