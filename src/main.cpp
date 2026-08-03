a3426dab620100facec3b24f2aca53acef1550e6
Adam Kurbiel <ad.kurbiel@gmail.com>
2026-08-03 12:25:02 +0200

    updated comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 /*
 <Copyright Adam Kurbiel 2026>
 
-Main file. Currently alot of things are stuck here and I need to find a way to move them in some single files.
-The best approach is to make SceneManager.
+Main file. Currently searching for some kind of scene/ui/render manager that will
+handle all updates, renders and checks.
 
 */
 #include "game.hpp"
 
 int main()
-{
+{	
 	Game game;
 	game.run();
 
