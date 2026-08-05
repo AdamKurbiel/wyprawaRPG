dabc97331d1a22dd184c910bc0bf5078a77e49fd
Adam Kurbiel <ad.kurbiel@gmail.com>
2026-08-05 17:06:55 +0200

    fixed syntax

--- src/game.hpp
+++ src/game.hpp
@@ -13,5 +13,5 @@ class Game
 {
     public:
         const float TILE_SIZE = 48.f;
-        void run(ResourceManager& rm, sf::RenderWindow&, Character character;
+        void run(ResourceManager& rm, sf::RenderWindow&, Character character);
 };
