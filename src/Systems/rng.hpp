b5f153ee7586ab6523787ea7a9190cae5db97c3c
Adam Kurbiel <ad.kurbiel@gmail.com>
2026-08-09 20:01:44 +0200

    added header file and instruction comments for RNG

---
+++ src/Systems/rng.hpp
@@ -0,0 +1,12 @@
+/*
+TBD.
+
+RNG class contains random number generator function with set seed on the
+start of the program.
+in future seed is stored and loaded in the save file.
+*/
+#pragma once
+
+class rng{
+
+};
